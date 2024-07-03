e45574f530d2e7ffb291e05619d346bb73794a66
David Yoder <david.yoder@live.com>
2024-07-02 20:15:08 -0400

    another fix maybe, moving html to root

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,17 @@
 import { defineConfig } from 'vite'
 
-   export default defineConfig({
-     server: {
-       proxy: {
-         '/.netlify/functions': {
-           target: 'http://localhost:9999',
-           changeOrigin: true,
-         },
-       },
-     },
-   })
+export default defineConfig({
+  root: '.',
+  build: {
+    outDir: 'dist',
+  },
+  server: {
+    proxy: {
+      '/.netlify/functions': {
+        target: 'http://localhost:9999',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/\.netlify\/functions/, ''),
+      },
+    },
+  },
+})
